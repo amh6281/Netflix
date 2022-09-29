b4634815d439fd740e1ce06f83b12023b8d065ee
amh6281 <amh6281@naver.com>
2022-09-29 12:05:46 +0900

    feat: Navbar 생성 2022.09.29
    pageYOffset이 0이 아닐경우 Navbar 색상이 검은색으로 변경되며, position은 fixed 옵션을 통해 고정시킨다.

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,8 +1,34 @@
 import React from "react";
+import Navbar from "../components/Navbar/Navbar";
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <Navbar />
+      <img
+        width="100%"
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+      <img
+        width="100%"
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+      <img
+        width="100%"
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+
+      <img
+        width="100%"
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default Home;
