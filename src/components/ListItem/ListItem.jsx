65b8c8caa497e46174d7b83a6dbaeddf4bc83036
amh6281 <amh6281@naver.com>
2022-09-30 11:12:28 +0900

    feat: 트레일러 hover 효과 2022.09.30
    영화 이미지 hover시 트레일러가 재생된다.

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -4,24 +4,48 @@ import {
   ThumbDownAltOutlined,
   ThumbUpAltOutlined,
 } from "@material-ui/icons";
-import React from "react";
+import React, { useState } from "react";
 import "./listItem.scss";
 
-const ListItem = () => {
+const ListItem = ({ index }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const trailer =
+    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+
   return (
-    <div className="listItem">
+    <div
+      className="listItem"
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <img
         src="https://user-images.githubusercontent.com/83646986/193170397-73ff8552-5ece-430c-aeaa-d900a69a2c78.jpg"
         alt=""
       />
-      <div className="itemInfo">
-        <div className="icons">
-          <PlayArrow />
-          <Add />
-          <ThumbUpAltOutlined />
-          <ThumbDownAltOutlined />
-        </div>
-      </div>
+      {isHovered && (
+        <>
+          <video src={trailer} autoPlay={true} loop />
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayArrow className="icon" />
+              <Add className="icon" />
+              <ThumbUpAltOutlined className="icon" />
+              <ThumbDownAltOutlined className="icon" />
+            </div>
+            <div className="itemInfoTop">
+              <span>1시간 37분</span>
+              <span className="limit">+15</span>
+              <span>2020</span>
+            </div>
+            <div className="desc">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Praesentium hic rem eveniet error possimus, neque ex doloribus.
+            </div>
+            <div className="genre">액션/SF</div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
