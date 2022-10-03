93e48f0afab702e1a432a03ed4420cd60f4b56a8
amh6281 <amh6281@naver.com>
2022-10-03 09:37:23 +0900

    feat: Login페이지 추가 2022.10.03

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./app.scss";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
 import Watch from "./Pages/Watch/Watch";
 
 function App() {
-  return <Register />;
+  return <Login />;
 }
 
 export default App;
